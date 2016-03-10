3e788a27cb228bf9a4c5fcd61b34344222999eed
No3x <noex_home@yahoo.de>
2016-03-10 17:54:13 +0100

    Added .editorconfig.

--- src/test/java/sample/ControllerTest.java
+++ src/test/java/sample/ControllerTest.java
@@ -1,10 +1,10 @@
 package sample;
 
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.CoreMatchers.*;
-
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by czoeller on 09.03.16.
@@ -13,6 +13,6 @@ public class ControllerTest {
     @Test
     public void testSample() {
         Controller controller = new Controller();
-        assertThat( controller, notNullValue() );
+        assertThat(controller, notNullValue());
     }
 }
--- src/test/java/sample/SampleTest.java
+++ src/test/java/sample/SampleTest.java
@@ -1,16 +1,16 @@
 package sample;
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.CoreMatchers.*;
-
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class SampleTest {
 
     @Test
     public void testSample() {
-        int value = 2*2;
-        assertThat( "2*2=4", value, is( 4 ) );
+        int value = 2 * 2;
+        assertThat("2*2=4", value, is(4));
     }
 
 }
