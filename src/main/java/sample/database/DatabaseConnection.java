3d9a5e81c64a9f9cf445bcfacbdafccaac9578f4
No3x <noex_home@yahoo.de>
2016-03-30 20:06:30 +0200

    removed static

--- src/main/java/sample/database/DatabaseConnection.java
+++ src/main/java/sample/database/DatabaseConnection.java
@@ -4,10 +4,11 @@ package sample.database;
  * Created by czoeller on 26.03.16.
  */
 public class DatabaseConnection {
-    public static String CONNECTOR;
-    public static String PATH;
-    public static String USER;
-    public static String PASSWORD;
+
+    public String CONNECTOR;
+    public String PATH;
+    public String USER;
+    public String PASSWORD;
 
     public DatabaseConnection() {
         final String path = System.getProperty("user.dir") + "/database.db";
