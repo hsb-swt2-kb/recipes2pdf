97197ba440a7433b2dc890c101c705384d23f231
No3x <noex_home@yahoo.de>
2016-04-03 21:52:18 +0200

    Added comments for interface

--- src/main/java/sample/model/IRepositoy.java
+++ src/main/java/sample/model/IRepositoy.java
@@ -7,7 +7,30 @@ import java.util.Optional;
  * Created by czoeller on 02.04.16.
  */
 public interface IRepositoy<T> {
+
+    /**
+     * Returns all records of this entity. If you need to get a subset, look for variations of "find()".
+     *
+     * @return result list
+     */
     List<T> getAll();
+
+    /**
+     * Returns an Optional record with given id.
+     *
+     * @param id
+     * @return Optional<T> the optional record.
+     */
+
     Optional<T> findById(long id);
+
+    /**
+     * Find first occurence of record that fulfills the query.
+     *
+     * @param subQuery this is a set of conditions that normally follow the "where" clause. Example:
+     *                 <code>"department = ? and dob &gt ?"</code>. If this value is "*" and no parameters provided, then it behaves as {@link #getAll()}.
+     * @param params   list of parameters corresponding to the place holders in the subquery.
+     * @return instance of <code>LazyList<Model></code> containing results.
+     */
     Optional<T> findFirst(String subQuery, Object... params);
 }
