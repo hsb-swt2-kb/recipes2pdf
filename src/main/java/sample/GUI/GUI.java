0a6b0357b46162abdae574cd4283819574a7eb56
Tobias Stelter <tobias.stelter@outlook.com>
2016-04-26 21:34:06 +0200

    added skip button.

--- src/main/java/sample/GUI/GUI.java
+++ src/main/java/sample/GUI/GUI.java
@@ -7,18 +7,27 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class GUI extends Application {
 
+    private final String wrongPathException = "The path of the FXML-file is wrong.";
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) {
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample/GUI/Default.fxml"));
         final ResourceBundle locale_bundle = ResourceBundle.getBundle("bundles/locale", Locale.getDefault());
         loader.setResources(locale_bundle);
-        Parent root = (Parent) loader.load();
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(wrongPathException);
+        }
         sample.gui.ControllerDefault ctrl = loader.getController();
 
         primaryStage.setTitle("Recipes2PDF");
