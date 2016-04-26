5449774b3263482185d7e1ba8ff3bb47809a6a89
Tobias Stelter <tobias.stelter@outlook.com>
2016-04-26 22:59:03 +0200

    added Datenbestand-Sub-Item to Kochbuch- and Rezepte-Tab, renamed manageRecipe to manageCookBook

--- src/main/java/sample/GUI/ControllerAddCookBook.java
+++ src/main/java/sample/GUI/ControllerAddCookBook.java
@@ -1,8 +1,19 @@
 package sample.gui;
 
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.event.*;
+
 public class ControllerAddCookBook {
 
+    @FXML
+    private Button skipButton;
 
+    @FXML
+    void skipWindow(ActionEvent event) {
+
+
+    }
 
 }
--- src/main/java/sample/gui/ControllerManageCookBook.java
+++ src/main/java/sample/gui/ControllerManageCookBook.java
@@ -3,5 +3,21 @@ package sample.gui;
 /**
  * Created by Tobias on 24.04.2016.
  */
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.event.*;
+
 public class ControllerManageCookBook {
+
+    @FXML
+    private Button export2pdfButton;
+
+    @FXML
+    void export2pdf(ActionEvent event) {
+        ControllerDefault controllerDefault = new ControllerDefault();
+        controllerDefault.newWindow("/sample/GUI/ExportCookBook.fxml");
+
+    }
+
 }
