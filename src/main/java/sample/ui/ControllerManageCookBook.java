ffa6e359cae623d29677e3a1f9ef21fbd6403a5d
Tobias Stelter <tobias.stelter@outlook.com>
2016-04-28 13:35:50 +0200

    Added Test-Data and Refresh-Method

--- src/main/java/sample/ui/ControllerManageCookBook.java
+++ src/main/java/sample/ui/ControllerManageCookBook.java
@@ -7,6 +7,9 @@ package sample.ui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.event.*;
+import javafx.scene.control.ListView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class ControllerManageCookBook {
 
@@ -16,6 +19,12 @@ public class ControllerManageCookBook {
     private final String delete = "Löschen";
 
     ControllerDefault controllerDefault = new ControllerDefault();
+
+    @FXML
+    private ListView<String> listViewRecipes;
+
+    @FXML
+    private ListView<String> listViewCookBook;
 
     @FXML
     private Button delteButtonCookBook;
@@ -33,9 +42,19 @@ public class ControllerManageCookBook {
 
     }
 
+    void refreshListViews(ObservableList<String> recipes, ObservableList<String> cookbook){
+        this.listViewRecipes.setItems(recipes);
+        this.listViewCookBook.setItems(cookbook);
+    }
+
     @FXML
     void delteRecipe(ActionEvent event) {
         controllerDefault.newWindow(deleteFXML, delete);
+
+        //Test
+        ObservableList<String> recipes = FXCollections.observableArrayList ("Chilli", "Pizza", "Eintopf", "Bohnenauflauf");
+        ObservableList<String> cookbook = FXCollections.observableArrayList ("Rindsschmorbraten");
+        refreshListViews(recipes, cookbook);
 
     }
 
