6ae9e25a84aae5fa8909839ef8d117e387c59d1e
Tobias Stelter <tobias.stelter@outlook.com>
2016-04-29 13:28:17 +0200

    added Browse-Icons

--- src/main/java/sample/ui/ControllerExportCookBook.java
+++ src/main/java/sample/ui/ControllerExportCookBook.java
@@ -3,5 +3,13 @@ package sample.ui;
 /**
  * Created by Tobias on 25.04.2016.
  */
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.event.*;
+
 public class ControllerExportCookBook {
+
+    @FXML
+    private Button browseButton;
 }
