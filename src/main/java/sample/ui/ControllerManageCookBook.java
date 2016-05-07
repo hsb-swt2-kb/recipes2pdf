90e3212f86e0e60844ffd63235f5ca6a04ed4e09
Tobias Stelter <tobias.stelter@outlook.com>
2016-05-07 18:02:28 +0200

    Added Search

--- src/main/java/sample/ui/ControllerManageCookBook.java
+++ src/main/java/sample/ui/ControllerManageCookBook.java
@@ -6,10 +6,12 @@ package sample.ui;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 
@@ -25,28 +27,51 @@ public class ControllerManageCookBook {
     private final String deleteText = "Löschen";
     private final String noElementSelectedText = "Error";
         private final String changeText = "Rezept ändern";
-
-
-
+    ObservableList<String> recipes;
+    ObservableList<String> cookbook;
     private ControllerDefault controllerDefault = new ControllerDefault();
-
     @FXML
     private ListView<String> listViewRecipes;
-
     @FXML
     private GridPane gridPane;
-
     @FXML
     private Button plusButton;
-
-
+    @FXML
+    private TextField searchFieldRecipes;
+    @FXML
+    private TextField searchFieldCookBooks;
     @FXML
     private ListView<String> listViewCookBook;
 
+    @FXML
+    private void initialize() {
+        /* TESTDATA */
+        this.recipes = FXCollections.observableArrayList("Chilli", "Pizza", "Eintopf", "Bohnenauflauf", "Rindsschmorbraten");
+        this.cookbook = FXCollections.observableArrayList("Rindsschmorbraten");
+         /* TESTDATA END */
+        refreshListViews(recipes, cookbook);
+        searchInListView(recipes, searchFieldRecipes, listViewRecipes);
+        searchInListView(cookbook, searchFieldCookBooks, listViewCookBook);
+    }
 
-    void refreshListViews(ObservableList<String> recipes, ObservableList<String> cookbook){
+    void refreshListViews(ObservableList<String> recipes, ObservableList<String> cookbook) {
         this.listViewRecipes.setItems(recipes);
         this.listViewCookBook.setItems(cookbook);
+    }
+
+
+    void searchInListView(ObservableList<String> list, TextField searchField, ListView<String> listView) {
+        FilteredList<String> filteredData = new FilteredList<>(list, s -> true);
+
+        searchField.textProperty().addListener(obs -> {
+            String filter = searchField.getText();
+            if (filter == null || filter.length() == 0) {
+                filteredData.setPredicate(s -> true);
+            } else {
+                filteredData.setPredicate(s -> s.contains(filter));
+            }
+            listView.setItems(filteredData);
+        });
     }
 
     @FXML
@@ -54,10 +79,6 @@ public class ControllerManageCookBook {
         controllerDefault.newWindowNotResizable(deleteFXML, deleteText);
         controllerDefault.newWindowNotResizable(noElementSelectedFXML, noElementSelectedText);
 
-        //Test
-        ObservableList<String> recipes = FXCollections.observableArrayList ("Chilli", "Pizza", "Eintopf", "Bohnenauflauf","Rindsschmorbraten");
-        ObservableList<String> cookbook = FXCollections.observableArrayList ("Rindsschmorbraten");
-        refreshListViews(recipes, cookbook);
 
     }
 
