6cf6344adaf652a82692e13e9498c7a97c863be5
Tobias Stelter <tobias.stelter@outlook.com>
2016-05-13 13:52:25 +0200

    added search 4 cookbooks

--- src/main/java/sample/ui/ControllerManageCookBooks.java
+++ src/main/java/sample/ui/ControllerManageCookBooks.java
@@ -5,9 +5,13 @@ package sample.ui;
  */
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class ControllerManageCookBooks {
@@ -20,9 +24,33 @@ public class ControllerManageCookBooks {
     @FXML
     private Button closeButton;
 
+    @FXML
+    private TextField searchFieldCookBooks;
+
+
+    @FXML
+    private ListView<String> listViewCookBooks;
+
 
     @FXML
     private Button changeButton;
+
+    private ObservableList<String> cookbooks;
+
+    @FXML
+    private void initialize() {
+        /* TESTDATA */
+        this.cookbooks = FXCollections.observableArrayList("Tobias Kochbuch", "Henriks Kochbuch", "Florians Kochbuch", "Danys Kochbuch", "Christians Kochbuch", "Markuss Kochbuch", "Kais Kochbuch");
+         /* TESTDATA END */
+        refreshListViews(cookbooks);
+    }
+
+    private void refreshListViews(ObservableList<String> cookbooks) {
+        FXCollections.sort(cookbooks);
+        this.listViewCookBooks.setItems(cookbooks);
+        ControllerManageCookBook controllerManageCookBook = new ControllerManageCookBook();
+        controllerManageCookBook.searchInListView(cookbooks, searchFieldCookBooks, listViewCookBooks);
+    }
 
     @FXML
     void changeCookBook(ActionEvent event) {
@@ -35,6 +63,7 @@ public class ControllerManageCookBooks {
     void closeWindow(ActionEvent event) {
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
+
 
     }
 
