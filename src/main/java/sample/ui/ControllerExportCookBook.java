344296814de211945991b2036b6feff139ac63be
Tobias Stelter <tobias.stelter@outlook.com>
2016-05-14 15:54:19 +0200

    display choice in die FormatMenu (A4,A5)

--- src/main/java/sample/ui/ControllerExportCookBook.java
+++ src/main/java/sample/ui/ControllerExportCookBook.java
@@ -8,6 +8,8 @@ package sample.ui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
 public class ControllerExportCookBook {
@@ -19,7 +21,28 @@ public class ControllerExportCookBook {
     private Button saveButton;
 
     @FXML
+    private MenuButton menuButtonFormat;
+
+    @FXML
+    private MenuItem menuItemA5;
+
+    @FXML
+    private MenuItem menuItemA4;
+
+
+
+    @FXML
     private Button closeButton;
+
+    @FXML
+    void chooseA4(ActionEvent event) {
+        menuButtonFormat.setText("A4");
+    }
+
+    @FXML
+    void chooseA5(ActionEvent event) {
+        menuButtonFormat.setText("A5");
+    }
 
     @FXML
     void closeWindow(ActionEvent event) {
