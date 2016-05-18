96003da409d6302819b8d12e8e1e6c6f3edcf499
Markus <klugem89@web.de>
2016-05-18 16:16:59 +0200

    typo

--- src/main/java/sample/ui/UI.java
+++ src/main/java/sample/ui/UI.java
@@ -1,7 +1,20 @@
 package sample.ui;
-
+import sample.model.ICookbook;
 /**
  * Created by markus on 18.05.16.
  */
-public interface UI {
+public interface UI
+{
+    void      start(String[] parameter);
+    void      addRecipes(String dirPath);
+    void      addRecipes(String[] filenamesWithPath);
+    void      delRecipes(String[] recipeNames);
+    void      showRecipe(String recipeName);
+    void      editRecipe(String recipeName);
+    void      createCookBook(String cookBookName,String pictureFileName,String preamble);
+    String    showCookBook(String cookBookName);
+    ICookbook editCookBook(String cookBookName,String keyAndValue);
+    void      delCookBook(String cookBookName);
+    void      export(String cookBookName,String fileType,String format);
+    //void    export(String recipeName,String fileType,String format);
 }
