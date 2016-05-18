1901b6307e83ef59db041af6aebb048e39ad2647
Markus <klugem89@web.de>
2016-05-18 16:16:11 +0200

    UI Interface angelegt.

--- src/main/java/sample/ui/UI.java
+++ src/main/java/sample/ui/UI.java
@@ -1,19 +1,7 @@
-//package ICookbook.model;
+package sample.ui;
+
 /**
- * UI : Interface für die verschiedenen UIs.
+ * Created by markus on 18.05.16.
  */
-public interface UI
-{
-  void      start(String[] parameter);
-  void      addRecipes(String dirPath);
-  void      addRecipes(String[] filenamesWithPath);
-  void      delRecipes(String[] recipeNames);
-  void      showRecipe(String recipeName);
-  void      editRecipe(String recipeName);
-  void      createCookBook(String cookBookName,String pictureFileName,String preamble);
-  String    showCookBook(String cookBookName);
-  //ICookBook editCookBook(String cookBookName,String keyAndValue);
-  void      delCookBook(String cookBookName);
-  void      export(String cookBookName,String fileType,String format);
-  //void    export(String recipeName,String fileType,String format);
+public interface UI {
 }
