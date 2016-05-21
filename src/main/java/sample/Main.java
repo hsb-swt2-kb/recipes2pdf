7a9280ee0b879ed203b254df0ba3abbe9a77a175
Markus <klugem89@web.de>
2016-05-21 18:25:39 +0200

    :)

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -2,17 +2,11 @@ package sample;
 
 import sample.ui.GUI;
 
-<<<<<<< HEAD
 /**
  * The Main starts the program.
  */
-
-public class Main {
-
-=======
 public class Main
 {
->>>>>>> parser
     public static void main(String[] args) {
         new GUI().start();
     }
