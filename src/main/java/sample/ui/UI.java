c06a943304476ad78ea82b4a56f8cf9c657838d8
Markus <klugem89@web.de>
2016-05-23 13:06:51 +0200

    Whitespaces in UI

--- src/main/java/sample/ui/UI.java
+++ src/main/java/sample/ui/UI.java
@@ -1,88 +1,88 @@
-package sample.ui; 
- 
-import sample.database.dao.CookbookDAO; 
-import sample.database.dao.RecipeDAO; 
-import sample.model.Cookbook; 
-import sample.model.ICookbook; 
-import sample.model.Recipe; 
-import sample.parser.Parser; 
- 
-import java.io.*; 
-import java.util.ArrayList; 
- 
-/** 
- * Created by markus on 18.05.16. 
- */ 
-abstract class UI { 
-    abstract void      start         (String[] parameter); 
- 
-    void      addRecipes    (String   dirPath){ 
-        File file=new File(dirPath); 
-        for(File f : file.listFiles()){ 
-            addRecipes(f.getAbsolutePath()); 
-        } 
-    } 
- 
-    void      addRecipes    (String[] filenamesWithPath){ 
-        try{ 
-            Parser parser = new Parser(); 
-            for(int i=0;i<filenamesWithPath.length;i++){ 
-                File f = new File(filenamesWithPath[i]); 
-                Recipe recipe = parser.parse(f); 
-                new RecipeDAO().insert(recipe); 
-            } 
-        } 
-        catch(FileNotFoundException e) { 
-            // TODO: FileNotFoundException behandeln 
-        } 
-    } 
- 
-    void      delRecipes    (String[] recipeNames){ 
-        Recipe recipe = new Recipe(); 
-        for(String recipeName : recipeNames){ 
-            recipe.setTitle(recipeName); 
-            new RecipeDAO().delete(recipe); 
-        } 
-    } 
- 
-    void      showRecipe    (String   recipeName){ 
- 
- 
-    } 
- 
-    void      editRecipe    (String   reciepeName){ 
- 
- 
- 
-    } 
- 
-    void      createCookBook(String   cookBookName,String pictureFileName,String preamble){ 
-        new Cookbook().setTitle(cookBookName); 
-        //cookbook.setPicture(pictureFileName); 
-        //cookbook.setPreamble(preamble); 
-    } 
- 
-    protected ArrayList<String> readFile(String file) throws IOException { 
-        BufferedReader reader = new BufferedReader(new FileReader(file)); 
-        String         line = null; 
-        ArrayList<String> lines = new ArrayList<String>(); 
- 
-        try { 
-            while((line = reader.readLine()) != null) { 
-                lines.add(line); 
-            } 
- 
-            return lines; 
-        } 
-        finally { 
-            reader.close(); 
-        } 
-    } 
-    void      showCookBook  (String   cookBookName){ 
- 
-    } 
- 
-    ICookbook editCookBook  (String cookBookName,String keyAndValue){ 
-        return null; 
-    } 
-} 
+package sample.ui;
+
+import sample.database.dao.CookbookDAO;
+import sample.database.dao.RecipeDAO;
+import sample.model.Cookbook;
+import sample.model.ICookbook;
+import sample.model.Recipe;
+import sample.parser.Parser;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * Created by markus on 18.05.16.
+ */
+abstract class UI {
+    abstract void      start         (String[] parameter);
+
+    void      addRecipes    (String   dirPath){
+        File file=new File(dirPath);
+        for(File f : file.listFiles()){
+            addRecipes(f.getAbsolutePath());
+        }
+    }
+
+    void      addRecipes    (String[] filenamesWithPath){
+        try{
+            Parser parser = new Parser();
+            for(int i=0;i<filenamesWithPath.length;i++){
+                File f = new File(filenamesWithPath[i]);
+                Recipe recipe = parser.parse(f);
+                new RecipeDAO().insert(recipe);
+            }
+        }
+        catch(FileNotFoundException e) {
+            // TODO: FileNotFoundException behandeln
+        }
+    }
+
+    void      delRecipes    (String[] recipeNames){
+        Recipe recipe = new Recipe();
+        for(String recipeName : recipeNames){
+            recipe.setTitle(recipeName);
+            new RecipeDAO().delete(recipe);
+        }
+    }
+
+    void      showRecipe    (String   recipeName){
+
+
+    }
+
+    void      editRecipe    (String   reciepeName){
+
+
+
+    }
+
+    void      createCookBook(String   cookBookName,String pictureFileName,String preamble){
+        new Cookbook().setTitle(cookBookName);
+        //cookbook.setPicture(pictureFileName);
+        //cookbook.setPreamble(preamble);
+    }
+
+    protected ArrayList<String> readFile(String file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String         line = null;
+        ArrayList<String> lines = new ArrayList<String>();
+
+        try {
+            while((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+
+            return lines;
+        }
+        finally {
+            reader.close();
+        }
+    }
+    void      showCookBook  (String   cookBookName){
+
+    }
+
+    ICookbook editCookBook  (String cookBookName,String keyAndValue){
+        return null;
+    }
+}
