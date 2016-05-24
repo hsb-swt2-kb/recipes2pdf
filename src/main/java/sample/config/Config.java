0d679b66f0be49d116019a48db7410b3c39615e5
Kai Nortmann <kai.nortmann@gmail.com>
2016-05-24 19:57:32 +0200

    Added default Config

--- src/main/java/sample/config/Config.java
+++ src/main/java/sample/config/Config.java
@@ -1,6 +1,7 @@
 package sample.config;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -17,30 +18,22 @@ class Config implements IConfig {
     private Config() {
 
         properties = new Properties();
-
-        setProperties();
         try {
-            properties.store(new FileOutputStream(PROGRAM_USERDATA_DIR + File.separator + "config.txt"), "Properties");
+            properties.load(new FileInputStream(PROGRAM_USERDATA_DIR + File.separator + "config.cfg"));
         } catch (IOException e) {
-            e.printStackTrace();
+
+            try {
+                properties.load(this.getClass().getClassLoader().getResourceAsStream("sample/config/default_config.cfg"));
+                properties.store(new FileOutputStream(PROGRAM_USERDATA_DIR + File.separator + "config.txt"), "Properties");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
     public static Config getInstance() {
         return config;
     }
-
-    private void setProperties() {
-
-        properties.setProperty("TEMP_FOLDER_NAME", "temp");
-        properties.setProperty("IMAGE_FOLDER_NAME", "images");
-        properties.setProperty("TEMPLATE_FOLDER_NAME", "templates");
-        properties.setProperty("TEMPLATE_FILE_NAME", "cookbookTemplate.tex");
-        properties.setProperty("OUTPUT_FOLDER_NAME", "output");
-        properties.setProperty("OUTPUT_FILE_PREFIX", "recipe");
-        properties.setProperty("IMAGE_PREFIX", "image_");
-    }
-
 
     public String getProperty(String key) throws IllegalArgumentException {
         if (key.equalsIgnoreCase("PROGRAM_USERDATA_DIR")){
