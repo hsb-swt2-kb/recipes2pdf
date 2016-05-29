bbc735a0a6a8f99128290d784eac6bcb4c958b65
Kai Nortmann <kai.nortmann@gmail.com>
2016-05-29 18:20:21 +0200

    Fix create text tex template

--- src/test/java/sample/builder/PdfBuilderTest.java
+++ src/test/java/sample/builder/PdfBuilderTest.java
@@ -3,18 +3,15 @@ package sample.builder;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.input.NullInputStream;
 import org.junit.Test;
-import sample.builder.Exceptions.TemplateConverterException;
 import sample.builder.Exceptions.TexParserException;
 import sample.config.IConfig;
 import sample.model.*;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -85,16 +82,16 @@ public class PdfBuilderTest {
         r1.setRegion(grichenland);
         r1.setText("Rezepttext 1");
         r1.setTitle("Rezepttitel1");
-        r1.add("Zutat1",5,"g");
-        r1 = generateRecipe("Rezepttitel1","Rezepttext 1",1L,"Vorspeise","Griechenland","Frühling","Zutat1",5,"g");
+        r1.add("Zutat1", 5, "g");
+        r1 = generateRecipe("Rezepttitel1", "Rezepttext 1", 1L, "Vorspeise", "Griechenland", "Frühling", "Zutat1", 5, "g");
 
         r2.setCategory(nachspeise);
         r2.setSeason(sommer);
         r2.setRegion(mexiko);
         r2.setText("Rezepttext 2");
         r2.setTitle("Rezepttitel2");
-        r2.add("Zutat2",8,"g");
-        r2 = generateRecipe("Rezepttitel2","Rezepttext 2",2L,"Nachspeise","Mexiko","Sommer","Zutat2",8,"g");
+        r2.add("Zutat2", 8, "g");
+        r2 = generateRecipe("Rezepttitel2", "Rezepttext 2", 2L, "Nachspeise", "Mexiko", "Sommer", "Zutat2", 8, "g");
 
 
         r3.setCategory(hauptspeise);
@@ -102,71 +99,70 @@ public class PdfBuilderTest {
         r3.setRegion(china);
         r3.setText("Rezepttext 3");
         r3.setTitle("Rezepttitel3");
-        r3.add("Zutat3",6,"g");
-        r3 = generateRecipe("Rezepttitel3","Rezepttext 3",3L,"Hauptspeise","China","Frühling","Zutat3",6,"g");
+        r3.add("Zutat3", 6, "g");
+        r3 = generateRecipe("Rezepttitel3", "Rezepttext 3", 3L, "Hauptspeise", "China", "Frühling", "Zutat3", 6, "g");
 
         r4.setCategory(vorspeise);
         r4.setSeason(winter);
         r4.setRegion(mexiko);
         r4.setText("Rezepttext 4");
         r4.setTitle("Rezepttitel4");
-        r4.add("Zutat4",4,"g");
-        r4 = generateRecipe("Rezepttitel4","Rezepttext 4",4L,"Vorspeise","Mexiko","Winter","Zutat4",4,"g");
-
+        r4.add("Zutat4", 4, "g");
+        r4 = generateRecipe("Rezepttitel4", "Rezepttext 4", 4L, "Vorspeise", "Mexiko", "Winter", "Zutat4", 4, "g");
 
 
         cookbook.addRecipe(r1);
         cookbook.addRecipe(r2);
         cookbook.addRecipe(r3);
         cookbook.addRecipe(r4);
-        ((Cookbook)cookbook).setSortlevel(generateSortlevelList("category","region"));
+        ((Cookbook) cookbook).setSortlevel(generateSortlevelList("category", "region"));
         pdfBuilder.build(cookbook);
-        String texFile= FileUtils.readFileToString(new File(config.getProperty("PROGRAM_USERDATA_DIR") + File.separator + config.getProperty("OUTPUT_FOLDER_NAME") + File.separator + cookbook.getTitle() + ".tex"));
+        String texFile = FileUtils.readFileToString(new File(config.getProperty("PROGRAM_USERDATA_DIR") + File.separator + config.getProperty("OUTPUT_FOLDER_NAME") + File.separator + cookbook.getTitle() + ".tex"));
 
         //Check generated text for r1
-        assertThat(texFile,containsString("\\chead{Vorspeise}"));
-        assertThat(texFile,containsString("\\lfoot{Vorspeise.Griechenland}"));
-        assertThat(texFile,containsString("\\rfoot{Vorspeise.Griechenland}"));
-        assertThat(texFile,containsString("\\item {Zutat1 5.0 g}"));
+        assertThat(texFile, containsString("\\chead{Vorspeise}"));
+        assertThat(texFile, containsString("\\lfoot{Vorspeise.Griechenland}"));
+        assertThat(texFile, containsString("\\rfoot{Vorspeise.Griechenland}"));
+        assertThat(texFile, containsString("\\item {Zutat1 5.0 g}"));
         List<String> substrings = new ArrayList<>();
         substrings.add("\\includegraphics[width=\\linewidth]{");
-        substrings.add(FilenameUtils.separatorsToUnix(System.getProperty("user.home")) +"/.recipes2pdf/images/Rezepttitel11" );
-        assertThat(texFile,stringContainsInOrder(substrings));
-        assertThat(texFile,containsString("Rezepttext 1"));
+        substrings.add(FilenameUtils.separatorsToUnix(System.getProperty("user.home")) + "/.recipes2pdf/images/Rezepttitel11");
+        assertThat(texFile, stringContainsInOrder(substrings));
+        assertThat(texFile, containsString("Rezepttext 1"));
 
         //Check generated text for r2
-        assertThat(texFile,containsString("\\chead{Nachspeise}"));
-        assertThat(texFile,containsString("\\lfoot{Nachspeise.Mexiko}"));
-        assertThat(texFile,containsString("\\rfoot{Nachspeise.Mexiko}"));
-        assertThat(texFile,containsString("\\item {Zutat2 8.0 g}"));
+        assertThat(texFile, containsString("\\chead{Nachspeise}"));
+        assertThat(texFile, containsString("\\lfoot{Nachspeise.Mexiko}"));
+        assertThat(texFile, containsString("\\rfoot{Nachspeise.Mexiko}"));
+        assertThat(texFile, containsString("\\item {Zutat2 8.0 g}"));
         substrings.clear();
         substrings.add("\\includegraphics[width=\\linewidth]{");
-        substrings.add(FilenameUtils.separatorsToUnix(System.getProperty("user.home")) +"/.recipes2pdf/images/Rezepttitel22" );
-        assertThat(texFile,stringContainsInOrder(substrings));
-        assertThat(texFile,containsString("Rezepttext 2"));
+        substrings.add(FilenameUtils.separatorsToUnix(System.getProperty("user.home")) + "/.recipes2pdf/images/Rezepttitel22");
+        assertThat(texFile, stringContainsInOrder(substrings));
+        assertThat(texFile, containsString("Rezepttext 2"));
 
         //Check generated text for r3
-        assertThat(texFile,containsString("\\chead{Hauptspeise}"));
-        assertThat(texFile,containsString("\\lfoot{Hauptspeise.China}"));
-        assertThat(texFile,containsString("\\rfoot{Hauptspeise.China}"));
-        assertThat(texFile,containsString("\\item {Zutat3 6.0 g}"));
+        assertThat(texFile, containsString("\\chead{Hauptspeise}"));
+        assertThat(texFile, containsString("\\lfoot{Hauptspeise.China}"));
+        assertThat(texFile, containsString("\\rfoot{Hauptspeise.China}"));
+        assertThat(texFile, containsString("\\item {Zutat3 6.0 g}"));
         substrings.clear();
         substrings.add("\\includegraphics[width=\\linewidth]{");
-        substrings.add(FilenameUtils.separatorsToUnix(System.getProperty("user.home")) +"/.recipes2pdf/images/Rezepttitel33" );
-        assertThat(texFile,stringContainsInOrder(substrings));
-        assertThat(texFile,containsString("Rezepttext 3"));
+        substrings.add(FilenameUtils.separatorsToUnix(System.getProperty("user.home")) + "/.recipes2pdf/images/Rezepttitel33");
+        assertThat(texFile, stringContainsInOrder(substrings));
+        assertThat(texFile, containsString("Rezepttext 3"));
 
 
         //Check generated text for r4
-        assertThat(texFile,containsString("\\chead{Vorspeise}"));
-        assertThat(texFile,containsString("\\lfoot{Vorspeise.Mexiko}"));
-        assertThat(texFile,containsString("\\rfoot{Vorspeise.Mexiko}"));
-        assertThat(texFile,containsString("\\item {Zutat4 4.0 g}"));
+        assertThat(texFile, containsString("\\chead{Vorspeise}"));
+        assertThat(texFile, containsString("\\lfoot{Vorspeise.Mexiko}"));
+        assertThat(texFile, containsString("\\rfoot{Vorspeise.Mexiko}"));
+        assertThat(texFile, containsString("\\item {Zutat4 4.0 g}"));
         substrings.clear();
         substrings.add("\\includegraphics[width=\\linewidth]{");
-        substrings.add(FilenameUtils.separatorsToUnix(System.getProperty("user.home")) +"/.recipes2pdf/images/Rezepttitel44" );
-        assertThat(texFile,stringContainsInOrder(substrings));
-        assertThat(texFile,containsString("Rezepttext 4"));
+        substrings.add(FilenameUtils.separatorsToUnix(System.getProperty("user.home")) + "/.recipes2pdf/images/Rezepttitel44");
+        assertThat(texFile, stringContainsInOrder(substrings));
+        assertThat(texFile, containsString("Rezepttext 4"));
     }
 
     @Test
@@ -174,49 +170,50 @@ public class PdfBuilderTest {
         IConfig config = IConfig.getInstance();
         IConcreteBuilder pdfBuilder = new PdfBuilder(config);
 
-        IRecipe r1 = generateRecipe("Testrezept", "Rezepttext 1", 1L,"Vorspeise","Griechenland","Frühling","Zutat1",5,"g");
-        List<ISortlevel> sortlevels= generateSortlevelList("category","region","season");
-        pdfBuilder.build(r1,sortlevels);
-        String texFile= FileUtils.readFileToString(new File(config.getProperty("PROGRAM_USERDATA_DIR") + File.separator + config.getProperty("OUTPUT_FOLDER_NAME") + File.separator + r1.getTitle() + ".tex"));
+        IRecipe r1 = generateRecipe("Testrezept", "Rezepttext 1", 1L, "Vorspeise", "Griechenland", "Frühling", "Zutat1", 5, "g");
+        List<ISortlevel> sortlevels = generateSortlevelList("category", "region", "season");
+        pdfBuilder.build(r1, sortlevels);
+        String texFile = FileUtils.readFileToString(new File(config.getProperty("PROGRAM_USERDATA_DIR") + File.separator + config.getProperty("OUTPUT_FOLDER_NAME") + File.separator + r1.getTitle() + ".tex"));
 
         //Check generated Text
-        assertThat(texFile,containsString("\\chead{Vorspeise}"));
-        assertThat(texFile,containsString("\\lfoot{Vorspeise.Griechenland.Frühling}"));
-        assertThat(texFile,containsString("\\rfoot{Vorspeise.Griechenland.Frühling}"));
-        assertThat(texFile,containsString("\\item {Zutat1 5.0 g}"));
+        assertThat(texFile, containsString("\\chead{Vorspeise}"));
+        assertThat(texFile, containsString("\\lfoot{Vorspeise.Griechenland.Frühling}"));
+        assertThat(texFile, containsString("\\rfoot{Vorspeise.Griechenland.Frühling}"));
+        assertThat(texFile, containsString("\\item {Zutat1 5.0 g}"));
         List<String> substrings = new ArrayList<>();
         substrings.add("\\includegraphics[width=\\linewidth]{");
         substrings.add(FilenameUtils.separatorsToUnix(System.getProperty("user.home")) + "/.recipes2pdf/images/Testrezept1");
-        assertThat(texFile,stringContainsInOrder(substrings));
-        assertThat(texFile,containsString("Rezepttext 1"));
+        assertThat(texFile, stringContainsInOrder(substrings));
+        assertThat(texFile, containsString("Rezepttext 1"));
 
     }
 
     @Test(expected = TexParserException.class)
-    public void testThrowConverterEsception () throws  Exception{
+    public void testThrowConverterEsception() throws Exception {
         IConfig config = IConfig.getInstance();
-        config.setProperty("TEMPLATE_FILE_NAME","testTexFile.tex");
-        InputStream templateStrem = new NullInputStream(30);
-        File templateFile  = new File(config.getProperty("PROGRAM_USERDATA_DIR") + File.separator + "templates" + File.separator + "testTexFile.tex");
+        config.setProperty("TEMPLATE_FILE_NAME", "testTexFile.tex");
+        InputStream templateStream = new NullInputStream(30);
+        File templateFile = new File(config.getProperty("PROGRAM_USERDATA_DIR") + File.separator + "templates" + File.separator + "testTexFile.tex");
         TexParserException ex = null;
 
         try {
-            Files.copy(templateStrem, templateFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            FileUtils.copyInputStreamToFile(templateStream, templateFile);
             PdfBuilder builder = new PdfBuilder(config);
             ICookbook cookbook = new Cookbook();
-            cookbook.addRecipe(generateRecipe("Testrezept","Testrezepttext",1L,"Hauptspeise","Deutschland","Sommer","Mehl",400,"g"));
+            cookbook.addRecipe(generateRecipe("Testrezept", "Testrezepttext", 1L, "Hauptspeise", "Deutschland", "Sommer", "Mehl", 400, "g"));
             builder.build(cookbook);
         } catch (TexParserException e) {
             ex = e;
         } finally {
-            config.setProperty("TEMPLATE_FILE_NAME","cookbookTemplate.tex");
+            config.setProperty("TEMPLATE_FILE_NAME", "cookbookTemplate.tex");
+            IOUtils.closeQuietly(templateStream);
             throw ex;
         }
 
     }
 
     @Test(expected = TexParserException.class)
-    public void testThrowParserEsception () throws Exception {
+    public void testThrowParserEsception() throws Exception {
         IConfig config = IConfig.getInstance();
         PdfBuilder builder = new PdfBuilder(config);
         ICookbook cookbook = new Cookbook();
@@ -225,29 +222,29 @@ public class PdfBuilderTest {
     }
 
     @Test
-    public void testRicipeNoSortlevel () throws Exception {
+    public void testRicipeNoSortlevel() throws Exception {
         IConfig config = IConfig.getInstance();
         PdfBuilder builder = new PdfBuilder(config);
         ICookbook cookbook = new Cookbook();
-        IRecipe r1 = generateRecipe("Testrezept", "Rezepttext 1", 1L,"Vorspeise","Griechenland","Frühling","Zutat1",5,"g");
+        IRecipe r1 = generateRecipe("Testrezept", "Rezepttext 1", 1L, "Vorspeise", "Griechenland", "Frühling", "Zutat1", 5, "g");
 
         builder.build(r1);
-        String texFile= FileUtils.readFileToString(new File(config.getProperty("PROGRAM_USERDATA_DIR") + File.separator + config.getProperty("OUTPUT_FOLDER_NAME") + File.separator + r1.getTitle() + ".tex"));
+        String texFile = FileUtils.readFileToString(new File(config.getProperty("PROGRAM_USERDATA_DIR") + File.separator + config.getProperty("OUTPUT_FOLDER_NAME") + File.separator + r1.getTitle() + ".tex"));
 
         //Check generated Text
-        assertThat(texFile,containsString("\\chead{}"));
-        assertThat(texFile,containsString("\\lfoot{}"));
-        assertThat(texFile,containsString("\\rfoot{}"));
-        assertThat(texFile,containsString("\\item {Zutat1 5.0 g}"));
+        assertThat(texFile, containsString("\\chead{}"));
+        assertThat(texFile, containsString("\\lfoot{}"));
+        assertThat(texFile, containsString("\\rfoot{}"));
+        assertThat(texFile, containsString("\\item {Zutat1 5.0 g}"));
         List<String> substrings = new ArrayList<>();
         substrings.add("\\includegraphics[width=\\linewidth]{");
         substrings.add(FilenameUtils.separatorsToUnix(System.getProperty("user.home")) + "/.recipes2pdf/images/Testrezept1");
-        assertThat(texFile,stringContainsInOrder(substrings));
-        assertThat(texFile,containsString("Rezepttext 1"));
+        assertThat(texFile, stringContainsInOrder(substrings));
+        assertThat(texFile, containsString("Rezepttext 1"));
     }
 
     @Test
-    public void testBuildsMethod () throws Exception {
+    public void testBuildsMethod() throws Exception {
         IConfig config = IConfig.getInstance();
         PdfBuilder builder = new PdfBuilder(config);
         assertTrue(builder.builds("pdf"));
@@ -256,8 +253,7 @@ public class PdfBuilderTest {
 
     }
 
-    IRecipe generateRecipe(String title, String text, Long id, String category, String region,String season, String ingredientName, Integer ingrentAmount, String ingredientUnit)
-    {
+    IRecipe generateRecipe(String title, String text, Long id, String category, String region, String season, String ingredientName, Integer ingrentAmount, String ingredientUnit) {
         IRecipe recipe = new Recipe();
         ICategory testCategory = new Category();
         IRegion testRegion = new Region();
@@ -273,26 +269,26 @@ public class PdfBuilderTest {
         recipe.setCategory(testCategory);
         recipe.setRegion(testRegion);
         recipe.setSeason(testSeason);
-        recipe.add(ingredientName,ingrentAmount,ingredientUnit);
+        recipe.add(ingredientName, ingrentAmount, ingredientUnit);
         return recipe;
     }
 
-    List<ISortlevel> generateSortlevelList(String primaryLevel, String secondaryLevel, String thirdLevel){
+    List<ISortlevel> generateSortlevelList(String primaryLevel, String secondaryLevel, String thirdLevel) {
         List<ISortlevel> sortlevelList = new ArrayList<>();
         sortlevelList.add(generateSortlevel(primaryLevel));
         sortlevelList.add(generateSortlevel(secondaryLevel));
         sortlevelList.add(generateSortlevel(thirdLevel));
-        return  sortlevelList;
+        return sortlevelList;
     }
 
-    List<ISortlevel> generateSortlevelList(String primaryLevel, String secondaryLevel){
+    List<ISortlevel> generateSortlevelList(String primaryLevel, String secondaryLevel) {
         List<ISortlevel> sortlevelList = new ArrayList<>();
         sortlevelList.add(generateSortlevel(primaryLevel));
         sortlevelList.add(generateSortlevel(secondaryLevel));
-        return  sortlevelList;
+        return sortlevelList;
     }
 
-    ISortlevel generateSortlevel(String sortlevelName){
+    ISortlevel generateSortlevel(String sortlevelName) {
         ISortlevel sortlevel = new Sortlevel();
         sortlevel.setName(sortlevelName);
         return sortlevel;
