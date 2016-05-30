9b42a0aef955d2b2d932686dc517e68c7503fbfa
kinith <florianpfennig@gmx.de>
2016-05-30 17:22:16 +0200

    FIX recipe = null

--- src/main/java/sample/parser/ChefkochParser.java
+++ src/main/java/sample/parser/ChefkochParser.java
@@ -22,7 +22,6 @@ import java.util.regex.Pattern;
  * Chefkoch parser to parse recipes from chefkoch.de
  */
 public class ChefkochParser extends AConcreteParser {
-    private Recipe recipe = new Recipe();
 
     //private ChefkochAPI chefkochAPI;
 
@@ -46,6 +45,8 @@ public class ChefkochParser extends AConcreteParser {
      */
     @Override
     public Recipe parse(final ArrayList<String> text) throws Exception {
+        recipe = new Recipe();
+
         String format = "";
 
         for (String entry: text) {
