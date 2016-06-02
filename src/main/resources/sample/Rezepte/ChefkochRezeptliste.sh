3d867df677b02db65dab94ce4d7a437deef9c321
Markus <klugem89@web.de>
2016-06-02 08:01:40 +0200

    added recipe files in src/main/recources/sample

---
+++ src/main/resources/sample/Rezepte/ChefkochRezeptliste.sh
@@ -0,0 +1,22 @@
+#grep chefkoch bookmarks.html | grep /rezepte/ | sed 's/^.*HREF="//' | sed 's/".*//' >> Dokumente/Kochbuch/src/build/resources/main/sample/Rezepte/Rezeptliste.txt 
+wget http://www.chefkoch.de/rezepte/1086021214483450/Avocado-Tomaten-Mozzarella-Salat.html
+wget http://www.chefkoch.de/rezepte/1100581216028344/Das-beste-Chili-con-Carne.html
+wget http://www.chefkoch.de/rezepte/1114421217510354/Gyros-Nudelauflauf.html
+wget http://www.chefkoch.de/rezepte/1237791228742728/Pesto.html
+wget http://www.chefkoch.de/rezepte/1465951251467301/Hack-Reis-Auflauf.html
+wget http://www.chefkoch.de/rezepte/1493511254345417/Bratkartoffeln-aus-dem-Backofen.html
+wget http://www.chefkoch.de/rezepte/1646311272279276/Avocado-Guacamole.html
+wget http://www.chefkoch.de/rezepte/1761961285878272/Nasi-Goreng.html
+wget http://www.chefkoch.de/rezepte/1877541305451157/Original-indisches-Chicken-Curry-aus-Pune.html
+wget http://www.chefkoch.de/rezepte/19371005029738/Gruenkohl-mit-Pinkel-nach-Bremer-Art.html
+wget http://www.chefkoch.de/rezepte/1960501318939515/Reis-Hackfleischpfanne-mit-Paprika.html
+wget http://www.chefkoch.de/rezepte/1996611323342829/Mittelalterliches-knuspriges-frisch-gebackenes-Brot.html
+wget http://www.chefkoch.de/rezepte/492191143507674/Avocado-Aufstrich.html
+wget http://www.chefkoch.de/rezepte/607271160489273/Nudel-Schinken-Gratin.html
+wget http://www.chefkoch.de/rezepte/712781173941198/Cr-me-fraiche-Nudeln.html
+wget http://www.chefkoch.de/rezepte/750811177605167/Gruenkohl.html
+wget http://www.chefkoch.de/rezepte/772011180069862/Die-echte-Sauce-Bolognese.html
+wget http://www.chefkoch.de/rezepte/772011180069862/Die-echte-Sauce-Bolognese.html
+wget http://www.chefkoch.de/rezepte/842491189509147/Gratiniertes-Schweinefilet.html
+wget http://www.chefkoch.de/rezepte/906521195904259/Gefuellte-Avocado-mit-Olivenoel.html
+wget http://www.chefkoch.de/rezepte/1573761264920690/Vegetarisches-indisches-Blumenkohl-Tomaten-Curry.html
